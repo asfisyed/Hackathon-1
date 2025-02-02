52a4c1f225824aace3fb6d96465e1b048e538cdf
asfisyed <161502929+asfisyed@users.noreply.github.com>
2025-02-03 02:23:27 +0500

    submit milestone-3

---
+++ milestone 3/script.js
@@ -0,0 +1,36 @@
+var _a;
+// listing element
+(_a = document.getElementById('resumeForm')) === null || _a === void 0 ? void 0 : _a.addEventListener('submit', function (event) {
+    var _a;
+    event.preventDefault();
+    //Get reference to form elements using their IDs
+    var profilePictureInput = document.getElementById('profilePicture');
+    var nameElement = document.getElementById('name');
+    var emailElement = document.getElementById('email');
+    var phoneElement = document.getElementById('phone');
+    var educationElement = document.getElementById('education');
+    var experienceElement = document.getElementById('experience');
+    var skillsElement = document.getElementById('skills');
+    //check if all form elements are present
+    if (profilePictureInput && nameElement && emailElement && phoneElement && educationElement && experienceElement && skillsElement) {
+        var name_1 = nameElement.value;
+        var email = emailElement.value;
+        var phone = phoneElement.value;
+        var education = educationElement.value;
+        var experience = experienceElement.value;
+        var skills = skillsElement.value;
+        // picture elements 
+        var profilePictureFile = (_a = profilePictureInput.files) === null || _a === void 0 ? void 0 : _a[0];
+        var profilePictureURL = profilePictureFile ? URL.createObjectURL(profilePictureFile) : '';
+        var resumeOutput = "\n    <h2>Resume</h2>\n    ".concat(profilePictureURL ? "<img src=\"".concat(profilePictureURL, " alt=\"profile Picture \" class=\"profilePicture\">") : '', "\n    <p><strong>Name:</strong> ").concat(name_1, " </p>\n    <p><strong>Email:</strong> ").concat(email, " </p>\n    <p><strong>phone Number :</strong> ").concat(phone, " </p>\n\n    <h3>Education</h3>\n    <p>").concat(education, "</p>\n\n    <h3>Experience</h3>\n    <p>").concat(experience, "</p>\n\n    <h3>Skills</h3>\n    <p>").concat(skills, "</p>\n    ");
+        // resume output
+        var resumeOutputElement = document.getElementById('resumeOutput');
+        if (resumeOutputElement) {
+            resumeOutputElement.innerHTML = resumeOutput;
+            resumeOutputElement.style.display = 'block';
+        }
+    }
+    else {
+        console.error('One or more from elements are missing,');
+    }
+});
